cd63f4ed6685f3c3b097c79ff743aa342728ecc6
IVAR AHTSERHS <ravistha869@gmail.com>
2022-12-28 21:44:51 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function from running
+  const associateKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  associateKey.classList.add("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+function removeTransition(e) {
+  const associateKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  associateKey.classList.remove("playing");
+}
+window.addEventListener("keyup", removeTransition);
+window.addEventListener("keydown", playSound);
