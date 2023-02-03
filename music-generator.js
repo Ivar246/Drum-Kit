39d40b44455c0726ef4546d3abddefe9cc934566
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-03 22:37:15 +0545

    auto play feature(play random key)

---
+++ music-generator.js
@@ -0,0 +1,27 @@
+const music = [65, 83, 68, 70, 71, 72, 74, 75, 76];
+
+const playBtn = document.querySelector("#playBtn");
+
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  if (!audio) return; // stop the function from running
+  const associateKey = document.querySelector(`div[data-key="${keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  associateKey.classList.add("playing");
+  setTimeout(() => {
+    associateKey.classList.remove("playing");
+  }, 200);
+}
+
+let playing = false;
+
+playBtn.onclick = function (e) {
+  if (playing) return;
+  playing = true;
+  setInterval(() => {
+    const randomIndex = Math.floor(Math.random() * music.length);
+    const keyCode = music[randomIndex];
+    playSound(keyCode);
+  }, 200);
+};
